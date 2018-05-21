5c88310379d13675d1f4d7166cc55c89cfe9c1fc
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2018-05-21 15:50:16 +1200

    Structure app, add components

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,20 +2,25 @@ export const ADD_TODO = 'ADD_TODO';
 export const TOGGLE_TODO = 'TOGGLE_TODO';
 export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
 
+let nextTodoId = 0
 export const VisibilityFilters = {
   SHOW_ALL: 'SHOW_ALL',
   SHOW_COMPLETED: 'SHOW_COMPLETED',
   SHOW_ACTIVE: 'SHOW_ACTIVE',
 };
 
-export function addTodo(text) {
-  return { type: ADD_TODO, text };
-}
+export const addTodo = text => ({
+  type: 'ADD_TODO',
+  id: nextTodoId++,
+  text
+})
 
-export function toggleTodo(index) {
-  return { type: TOGGLE_TODO, index };
-}
+export const toggleTodo = id => ({
+   type: 'TOGGLE_TODO',
+   id
+})
 
-export function setVisibilityFilter(filter) {
-  return { type: SET_VISIBILITY_FILTER, filter };
-}
+export const setVisibilityFilter = filter => ({
+  type: 'SET_VISIBILITY_FILTER',
+  filter
+})
